fc7c75fc4bc85c81669df3eeee44bd3d9c26c8b0
Bankole Ebenezer <bankoleebenezer111@gmail.com>
2026-01-21 05:30:54 -0800

    update the main branch with this new changes

---
+++ database/assignment2.sql
@@ -0,0 +1,45 @@
+-- Query to insert a new record into the inventory table
+-- Task 1: Insert a new record to the account table
+INSERT INTO public.account (
+	account_firstname,
+	account_lastname,
+	account_email,
+	account_password
+) VALUES (
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
+
+-- Task 2: Modify the Tony Stark record to change the account_type to "Admin"
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- Task 3: Delete the Tony Stark record from the database
+DELETE FROM public.account
+WHERE account_email = 'tony@starkent.com';
+
+-- Task 4: Modify the "GM Hummer" record
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- Task 5: Use an inner join to select the make and model fields...
+SELECT
+	inv_make,
+	inv_model,
+	classification_name
+FROM
+	public.inventory
+INNER JOIN public.classification
+	ON public.inventory.classification_id = public.classification.classification_id
+WHERE
+	classification_name = 'Sport';
+
+-- Task 6: Update all records in the inventory table to add "/vehicles"...
+UPDATE public.inventory
+SET
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
